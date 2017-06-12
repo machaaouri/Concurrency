6d82fb628edf811cd82453739fb9127579fdcac7
Machaaouri, Houssame <Houssame.Machaaouri@misys.com>
2017-06-12 13:03:10 +0200

    create a possible dead lock situation

--- Concurrency/src/com/dead/locking/DeadLock.java
+++ Concurrency/src/com/dead/locking/DeadLock.java
@@ -6,6 +6,7 @@ public class DeadLock {
 	 * Creating a possible dead lock
 	 * if someone calls FuncA & FuncB at the same time
 	 */
+	
 	private Object  key1 =  new Object();
 	private Object  key2 =  new Object();
 	
